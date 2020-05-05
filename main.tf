e28497102ba86102a50fac8c2c51c8e8468fb51e
Kim, Hirokuni <yangkookkim@gmail.com>
2020-05-05 16:28:03 +0900

    Move to ap-northeast-1

--- main.tf
+++ main.tf
@@ -1,29 +1,38 @@
 # Specify the provider and access details
 provider "aws" {
-  region = "${var.aws_region}"
+  region  = "ap-northeast-1"
   profile = "default"
 }
 
-resource "aws_elb" "web" {
-  name = "terraform-example-elb"
-
-  # The same availability zone as our instances
-  availability_zones = aws_instance.web[*].availability_zone
-
-  listener {
-    instance_port     = 80
-    instance_protocol = "http"
-    lb_port           = 80
-    lb_protocol       = "http"
+terraform {
+  backend "s3" {
+    key      = "terraform.tfstate"
+    bucket   = "circleci-book-terraform-sample"
+    region   = "ap-northeast-1"
+    dynamodb_table = "terraform-state-lock"
   }
-
-  # The instances are registered automatically
-  instances = aws_instance.web[*].id
 }
+
+#resource "aws_elb" "web" {
+#  name = "terraform-example-elb"
+#
+#  # The same availability zone as our instances
+#  availability_zones = aws_instance.web[*].availability_zone
+#
+#  listener {
+#    instance_port     = 80
+#    instance_protocol = "http"
+#    lb_port           = 80
+#    lb_protocol       = "http"
+#  }
+#
+#  # The instances are registered automatically
+#  instances = aws_instance.web[*].id
+#}
 
 resource "aws_instance" "web" {
   instance_type = "m1.small"
-  ami           = "ami-039a49e70ea773ffc"
-
+  # Ubuntu 19.04 LTS AMI
+  ami           = "ami-07547ba969550e510"
   count = 1
 }

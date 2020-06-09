aa5968863a60f83966c1865d6fe3744859ce4150
Kim, Hirokuni <yangkookkim@gmail.com>
2020-06-10 08:13:55 +0900

    Remove state locking

--- main.tf
+++ main.tf
@@ -9,26 +9,8 @@ terraform {
     key            = "terraform.tfstate"
     bucket         = "circleci-book-terraform-sample"
     region         = "ap-northeast-1"
-    dynamodb_table = "terraform-state-lock"
   }
 }
-
-#resource "aws_elb" "web" {
-#  name = "terraform-example-elb"
-#
-#  # The same availability zone as our instances
-#  availability_zones = aws_instance.web[*].availability_zone
-#
-#  listener {
-#    instance_port     = 80
-#    instance_protocol = "http"
-#    lb_port           = 80
-#    lb_protocol       = "http"
-#  }
-#
-#  # The instances are registered automatically
-#  instances = aws_instance.web[*].id
-#}
 
 resource "aws_instance" "web" {
   instance_type = "m1.small"

82f0d519bbc40599ec1308fc719ec1519f17e3df
Kim, Hirokuni <yangkookkim@gmail.com>
2020-06-10 08:19:23 +0900

    State Lockingの導入
    - ロック用のDynamoDBを作成
    - State Lockingの有効化

--- main.tf
+++ main.tf
@@ -9,8 +9,26 @@ terraform {
     key            = "terraform.tfstate"
     bucket         = "circleci-book-terraform-sample"
     region         = "ap-northeast-1"
+    dynamodb_table = "terraform-state-lock-dynamo"
   }
 }
+
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name           = "terraform-state-lock-dynamo"
+  hash_key       = "LockID"
+  read_capacity  = 20
+  write_capacity = 20
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "DynamoDB Terraform State Lock Table"
+  }
+}
+
 
 resource "aws_instance" "web" {
   instance_type = "m1.small"

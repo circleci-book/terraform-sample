3147197fabaf113586c2d227989def6804be9e85
Kim, Hirokuni <yangkookkim@gmail.com>
2020-06-16 07:08:20 +0900

    Add DynamoDB

--- main.tf
+++ main.tf
@@ -12,6 +12,22 @@ terraform {
   }
 }
 
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name           = "terraform-state-lock-dynamo"
+  hash_key       = "LockID"
+  read_capacity  = 20
+  write_capacity = 20
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "DynamoDB Terraform State Lock Table"
+  }
+}
+
 resource "aws_instance" "web" {
   instance_type = "m1.small"
   # Ubuntu 19.04 LTS AMI
